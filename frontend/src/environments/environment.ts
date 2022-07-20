64f99967aed386ef8c5bf6542d01abab4feac2a4
Paul Nwoza Mensah <pnmensah@live.com>
2022-07-20 11:51:01 +0000

    Commit

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -9,6 +9,6 @@ export const environment = {
     url: 'fsnd-pmensah.us', // the auth0 domain prefix
     audience: 'coffeeshop', // the audience set for the auth0 app
     clientId: 'u3hB3OyxTOrnOD43wrPz4OHLjhfg0nih', // the client id generated for the auth0 app
-    callbackURL: 'http://127.0.0.1:3000', // the base url of the running ionic application. 
+    callbackURL: 'http://127.0.0.1:8100', // the base url of the running ionic application. 
   }
 };
